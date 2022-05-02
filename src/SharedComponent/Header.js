471bc6f01d4f102e3b8e9f48f2b9a9917defd9fb
Nayeem29 <nayeembracu29@gmail.com>
2022-05-03 04:02:25 +0600

    first commit

---
+++ src/SharedComponent/Header.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Header = () => {
+  const routes = [
+    { id: 1, name: 'Home', link: '/' },
+    { id: 1, name: 'Blogs', link: '/blogs' },
+    { id: 1, name: 'About', link: '/about' }
+  ];
+  const navLinkStyles = ({ isActive }) => {
+    return {
+      color: isActive ? 'red' : 'black',
+      fontSize: isActive ? '20px' : '20px',
+      fontWeight: isActive ? 'bold' : 'normal'
+    }
+  }
+  return (
+    <div>
+      <nav className='flex items-center justify-between'>
+        <div>
+          <p className='text-3xl font-semibold uppercase text-amber-400'>Simplex Perfumers</p>
+        </div>
+        <div>
+          <nav>
+            <ul className='md:flex md:items-center md:pb-0 pb-12'>
+              {
+                routes.map(route =>
+                  <li className='mr-8'>
+                    <NavLink style={navLinkStyles} key={route.id} to={route.link}>{route.name}</NavLink>
+                  </li>
+                )
+              }
+            </ul>
+          </nav>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
