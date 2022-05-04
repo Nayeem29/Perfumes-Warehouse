561cf5c3cee51251efd47ef97454557138c9839c
Nayeem29 <nayeembracu29@gmail.com>
2022-05-04 21:21:29 +0600

    login button and navbar updated

--- src/SharedComponent/Header.js
+++ src/SharedComponent/Header.js
@@ -1,5 +1,13 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { signOut } from 'firebase/auth';
+import React, { useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { NavLink, useNavigate } from 'react-router-dom';
+import auth from '../_firebase.init';
+import Spinner from './Spinner';
+import { FaSignOutAlt } from "react-icons/fa";
+import { FaSignInAlt } from "react-icons/fa";
+import { FaTimes } from "react-icons/fa";
+import { FaBars } from "react-icons/fa";
 
 const Header = () => {
   const routes = [
@@ -14,15 +22,25 @@ const Header = () => {
       fontWeight: isActive ? 'bold' : 'normal'
     }
   }
+  const [user, loading] = useAuthState(auth);
+  const [open, setOpen] = useState(false);
+  const navigate = useNavigate();
+  const logout = () => {
+    signOut(auth);
+    navigate('/login');
+  };
+  if (loading) {
+    return <Spinner />
+  }
   return (
-    <div className='my-4'>
+    <div className={`my-4 ${open ? 'h-44' : 'h-14'}`}>
       <nav className='flex items-center justify-between'>
         <div>
           <p className='text-3xl font-semibold uppercase text-amber-400 ml-4'>Simplex Perfumer</p>
         </div>
         <div>
-          <nav>
-            <ul className='md:flex md:items-center md:pb-0 pb-12'>
+          <nav className='md:flex items-center md:justify-end py-4 md:px-10 px-7'>
+            <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-20 left-0' : 'top-[-490px]'}`}>
               {
                 routes.map(route =>
                   <li className='mr-8'>
@@ -30,7 +48,21 @@ const Header = () => {
                   </li>
                 )
               }
+              {
+                user ?
+                  <button className='px-5' onClick={logout}><FaSignOutAlt className='text-xl' /></button>
+                  :
+                  <NavLink style={navLinkStyles} className='px-5' to='/login'><FaSignInAlt className='text-xl text-black inline' /></NavLink>
+              }
             </ul>
+            <div onClick={() => setOpen(!open)} className='text-2xl absolute right-8 top-6 cursor-pointer md:hidden'>
+              {
+                open ?
+                  <FaTimes />
+                  :
+                  <FaBars />
+              }
+            </div>
           </nav>
         </div>
       </nav>
