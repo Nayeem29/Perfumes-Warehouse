3201d38d8d0f685e218a252479d6ecd679508f74
Nayeem29 <nayeembracu29@gmail.com>
2022-05-05 22:55:51 +0600

    stock product form complete

--- src/Pages/Home/Inventory/Inventory.js
+++ src/Pages/Home/Inventory/Inventory.js
@@ -1,5 +1,3 @@
-import { jsonEval } from '@firebase/util';
-import { data } from 'autoprefixer';
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { toast, ToastContainer } from 'react-toastify';
@@ -15,48 +13,76 @@ const Inventory = () => {
       .then(data => setProduct(data))
   }, [id, product]);
   let { name, picture, price, quantity, supplier, about } = product;
+
+  const handleStockQuantity = (quantity) => {
+    product = { quantity };
+    const url = `http://localhost:5000/products/${id}`;
+    fetch(url, {
+      method: 'PUT',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(product)
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log('success', data);
+
+      })
+  }
   const handleQuantity = () => {
     // e.preventDefault();
     if (quantity > 0) {
       quantity--;
-      product = { quantity };
-      const url = `http://localhost:5000/products/${id}`;
-      fetch(url, {
-        method: 'PUT',
-        headers: {
-          'content-type': 'application/json'
-        },
-        body: JSON.stringify(product)
-      })
-        .then(res => res.json())
-        .then(data => {
-          console.log('success', data);
-          toast('Processed for delvering');
-        })
+      handleStockQuantity(quantity);
+      toast('Product is Delivering!!');
     } else {
-      toast('Not Available right now!');
+      toast('Not Available right now!!');
     }
   }
+  const handleStockForm = (e) => {
+    e.preventDefault();
+    const stock = e.target.stock.value;
+    quantity = parseInt(stock);
+    handleStockQuantity(quantity);
+    toast('Restock has been processed!!');
+  }
+
   return (
     <div className='my-20'>
       <div className='mb-16'>
         <hr className='h-2 w-full mb-8' />
-        <div className='grid grid-cols-2 md:grid-cols-3 gap-16 mx-4 mb-5'>
+        <div className='grid grid-cols-1 md:grid-cols-3 gap-16 mx-4 mb-5'>
           <div className='my-auto mx-10'>
             <img className='w-full h-1/2 rounded-lg' src={picture} alt="" />
           </div>
-          <div className='cols-span-2 mt-3 mx-auto font-semibold text-xl'>
-            <p className='py-2 my-3 text-4xl'>{name}</p>
-            <hr />
-            <p className='py-2 my-3'>Price: {price}</p>
-            <hr />
-            <p className='py-2 my-3'>Supplier: {supplier}</p>
-            <hr />
-            <p className='py-2 my-3'>Available: {quantity}</p>
-            <button onClick={() => handleQuantity()}
-              className='text-center px-10 py-3 my-3 bg-pink-300 text-black text-2xl font-semibold'>
-              DELIVERED
-            </button>
+          <div className='md:cols-span-2 mt-3 mx-auto font-semibold text-xl'>
+            <div className='md:flex items-center justify-around'>
+              <div>
+                <p className='py-2 my-3 text-4xl'>{name}</p>
+                <hr />
+                <p className='py-2 my-3'>Price: {price}</p>
+                <hr />
+                <p className='py-2 my-3'>Supplier: {supplier}</p>
+                <hr />
+                <p className='py-2 my-3'>Available: {quantity}</p>
+                <button onClick={() => handleQuantity()}
+                  className='text-center px-10 py-3 my-3 bg-pink-300 text-black text-2xl font-semibold'>
+                  DELIVERED
+                </button>
+              </div>
+              <div className='mx-auto md:mx-20'>
+                <form onSubmit={handleStockForm}>
+                  <label htmlFor="restock">Restock Product</label>
+                  <br />
+                  <input type="number" name="stock" id="" required />
+                  <br />
+                  <button
+                    className='text-center px-10 py-3 my-3 bg-pink-300 text-black text-2xl font-semibold'
+                  >Restock</button>
+                </form>
+              </div>
+            </div>
           </div>
         </div>
         <ToastContainer />
